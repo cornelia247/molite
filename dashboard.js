1e534f1b5cc32c64cf5738c06c53788f1d98931d
Ekpenyong Cornelia <ekpenyongcornelia2@gmail.com>
2020-12-19 20:56:13 +0100

    updated

--- dashboard.js
+++ dashboard.js
@@ -15,3 +15,30 @@ function closeSidebar() {
         sidebarOpen = false;
     }
 };
+const dropdown = () => {
+    const selectedAll = document.querySelectorAll(".selected");
+
+    selectedAll.forEach((selected) => {
+        const optionsContainer = selected.previousElementSibling;
+        const optionsList = optionsContainer.querySelectorAll(".option");
+        selected.addEventListener("click", () => {
+            if (optionsContainer.classList.contains("active")) {
+                optionsContainer.classList.remove("active");
+            } else {
+                let currentActive = document.querySelector(".options-container.active");
+                if (currentActive) {
+                    currentActive.classList.remove("active");
+                }
+                optionsContainer.classList.add("active");
+            }
+        });
+        optionsList.forEach(o => {
+            o.addEventListener("click", () => {
+                selected.innerHTML = o.querySelector("label").innerHTML;
+                optionsContainer.classList.remove("active");
+            });
+        });
+    });
+    
+};
+dropdown();
